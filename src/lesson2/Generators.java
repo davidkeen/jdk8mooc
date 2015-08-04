82e4525e8fba754e8bfe931899ae6d7386a79d5f
David Keen <dkkeen@gmail.com>
2015-08-04 15:09:56 +0100

    Improve mac generator.

--- src/lesson2/Generators.java
+++ src/lesson2/Generators.java
@@ -2,18 +2,16 @@ package lesson2;
 
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Generators {
 
     private static final Supplier<String> mobileSupplier = () -> String.format("447700900%03d", ThreadLocalRandom.current().nextInt(999));
 
-    private static final Supplier<String> macSupplier = () -> {
-        String[] s = Stream.generate(() -> String.format("%02x", ThreadLocalRandom.current().nextInt(255)))
-                .limit(6)
-                .toArray(String[]::new);
-        return String.join(":", s);
-    };
+    private static final Supplier<String> macSupplier = () -> ThreadLocalRandom.current().ints(6, 0, 255)
+            .mapToObj(i -> String.format("%02x", i))
+            .collect(Collectors.joining(":"));
 
     public static void main(String[] args) {
         System.out.println("Some mobile numbers:");
